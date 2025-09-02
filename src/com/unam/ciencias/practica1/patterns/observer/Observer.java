e41bbbfaacd26da62793c5d8a129c0a88c935267
Luis <luuiscoffe@luiscoffe>
2025-09-01 19:38:20 -0600

    Implementacion del patrón Observer

---
+++ src/com/unam/ciencias/practica1/patterns/observer/Observer.java
@@ -0,0 +1,25 @@
+package com.unam.ciencias.practica1.patterns.observer;
+
+/*
+ * Observer interface that defines the
+ * contract that all observers within
+ * the Observer pattern must follow.
+ * 
+ * Observers are notified by Subjects
+ * when a relevant event occurs.
+ */
+public interface Observer {
+
+
+    /*
+     * Method called when the subject notifies
+     * an event.
+     * 
+     * @param service Name of the service sending
+     * the notification.
+     * @param message Message sent by the service.
+     */
+    void update(String service, String message){
+        
+    }
+}
---
+++ src/com/unam/ciencias/practica1/patterns/observer/Subject.java
@@ -0,0 +1,48 @@
+package com.unam.ciencias.practica1.patterns.observer;
+
+/*
+ * Subject interface that defines the
+ * operations that observable subjects
+ * must implement within the Observer
+ * pattern.
+ * 
+ * A subject is observed by multiple
+ * objects (Observers) and is responsible
+ * for notifying them when an important
+ * event occurs.
+ */
+public interface Subject {
+
+    /*
+     * Method that registers a new observer in
+     * the observer list.
+     * 
+     * @param observer The observer you want to
+     * register.
+     */
+    void registerObserver(Observer observer){
+
+    }
+
+    /*
+     * Method that removes an observer from the 
+     * list of observers.
+     * 
+     * @param observer The observer you want to
+     * remove.
+     */
+    void removeObserver(Observer observer) {
+
+    }
+
+    /*
+     * Method that notifies all registered observers 
+     * with a message.
+     * 
+     * @param message The message to be sent to observers.
+     */
+    void notifyObserver(String message){
+
+    }
+
+}
