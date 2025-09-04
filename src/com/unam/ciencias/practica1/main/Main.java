9d1af97fd5c79e85f8b750b024853d7daf9a23ad
BecVal <cesarbecval@ciencias.unam.mx>
2025-09-04 12:12:59 -0600

    fix

--- src/com/unam/ciencias/practica1/main/Main.java
+++ src/com/unam/ciencias/practica1/main/Main.java
@@ -3,6 +3,6 @@ package com.unam.ciencias.practica1.main;
 public class Main {
     public static void main(String[] args) {
         Simulator simulator = new Simulator();
-        simulator.runSimulation();  // tu método que ejecuta la simulación mes a mes
+        simulator.runSimulation(); 
     }
 }
